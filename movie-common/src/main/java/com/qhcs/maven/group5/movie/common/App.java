8dece8d70ad47668737b5f3e87360e740689669e
Lewis185319 <1853199517@qq.com>
2018-02-07 17:27:10 +0800

    添加一个视频整体框架的项目

---
+++ movie-common/src/main/java/com/qhcs/maven/group5/movie/common/App.java
@@ -0,0 +1,13 @@
+package com.qhcs.maven.group5.movie.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ movie-service/src/main/java/com/qhcs/maven/group5/movie/service/App.java
@@ -0,0 +1,13 @@
+package com.qhcs.maven.group5.movie.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
